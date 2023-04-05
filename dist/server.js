f6630904b239e69dd7edc3efa9c8cc4f74d4f1b2
Renan Araújo <renan.araujoonline@gmail.com>
2023-04-05 15:11:05 -0300

    able to fetch /categories and /products from DB

--- dist/server.js
+++ dist/server.js
@@ -4,10 +4,48 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const mongodb_1 = require("mongodb");
 const app = (0, express_1.default)();
 const port = 3000;
+const DB_URI = 'mongodb+srv://admin:mdsadminkkk@clusterzero.ygwtxel.mongodb.net/menu';
+const client = new mongodb_1.MongoClient(DB_URI);
+let categories;
+let products;
+async function connect() {
+    try {
+        await client.connect();
+        console.log('Connected to MongoDB');
+        const db = client.db('menu');
+        categories = db.collection('categories');
+        products = db.collection('products');
+    }
+    catch (err) {
+        console.error(err);
+    }
+}
+connect();
 app.get('/', (req, res) => {
     res.send('Hello World!');
+});
+app.get('/products', async (req, res) => {
+    try {
+        const productList = await products.find().toArray();
+        res.send(productList);
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).send('Error retrieving products');
+    }
+});
+app.get('/categories', async (req, res) => {
+    try {
+        const categoryList = await categories.find().toArray();
+        res.send(categoryList);
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).send('Error retrieving categories');
+    }
 });
 app.listen(port, () => {
     console.log(`Server listening on port ${port}.`);
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,69 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { MongoClient, Collection } from 'mongodb';
 
 const app = express();
 const port = 3000;
 
+const DB_URI = 'mongodb+srv://admin:<senha>@clusterzero.ygwtxel.mongodb.net/menu';
+
+const client = new MongoClient(DB_URI);
+
+// interfaces -> mover pra outro arquivo dps
+
+interface Category{
+  id: string;
+  parent: Category | null;
+  name: string;
+}
+
+interface Product {
+  id: string;
+  categories: string[];
+  name: string;
+  qty: number;
+  price: number;
+}
+
+let categories: Collection<Category>;
+let products: Collection<Product>;
+
+async function connect() {
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB');
+    
+    const db = client.db('menu');
+    categories = db.collection<Category>('categories');
+    products = db.collection<Product>('products');
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+connect();
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/products', async (req: Request, res: Response) => {
+  try {
+    const productList = await products.find().toArray();
+    res.send(productList);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error retrieving products');
+  }
+});
+
+app.get('/categories', async (req: Request, res: Response) => {
+  try{
+    const categoryList = await categories.find().toArray();
+    res.send(categoryList);
+  } catch(err){
+    console.error(err);
+    res.status(500).send('Error retrieving categories');
+  }
 });
 
 app.listen(port, () => {
